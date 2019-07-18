665c11afc033d5d0b5bba1ed62951ff845ef5564
Henry Cooper <henrycooper@outlook.com>
2019-07-18 16:09:07 +0200

    added separator before stats. added advancedPreferencesViewControler. added new user defaults values

---
+++ BuildBuddy/Controllers/AdvancedPreferencesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AdvancedPreferencesViewController.swift
+//  BuildBuddy
+//
+//  Created by Henry Cooper on 18/07/2019.
+//  Copyright © 2019 Henry Cooper. All rights reserved.
+//
+
+import Cocoa
+
+class AdvancedPreferencesViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
