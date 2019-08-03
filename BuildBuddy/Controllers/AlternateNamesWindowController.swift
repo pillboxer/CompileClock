5f7cc609f314aeec2541cf34e15b8e0c2e2fcf95
Henry Cooper <henry@touchnote.com>
2019-08-03 15:37:48 +0100

    Created AlternateNamesWindowController. Changed CoreDataManager to automatically migrate. Added new menuItem for changing names. name getter is much more sophisticated

---
+++ BuildBuddy/Controllers/AlternateNamesWindowController.swift
@@ -0,0 +1,13 @@
+//
+//  AlternateNamesWindowController.swift
+//  BuildBuddy
+//
+//  Created by Henry Cooper on 03/08/2019.
+//  Copyright © 2019 Henry Cooper. All rights reserved.
+//
+
+import UIKit
+
+class AlternateNamesWindowController: NSWindowController {
+
+}
