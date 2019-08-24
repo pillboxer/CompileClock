894855d6cfea5bb9c5a185772569ffbe57fdb709
Henry Cooper <henry@touchnote.com>
2019-08-24 12:07:12 +0100

    Set appearance to always be light mode whilst working out bugs | Removed custom decimal place stuff | can now hide projects

---
+++ BuildBuddy/Controllers/ProjectPreferencesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProjectPreferencesViewController.swift
+//  BuildBuddy
+//
+//  Created by Henry Cooper on 23/08/2019.
+//  Copyright © 2019 Henry Cooper. All rights reserved.
+//
+
+import Cocoa
+
+class ProjectPreferencesViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
